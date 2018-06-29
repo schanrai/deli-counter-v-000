3631c5a840683bbee94fe92054fa92fc39aa1005
Sushi Chanrai <sushi@pathworks.co>
2018-06-29 02:20:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line (katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     katz_deli.each_with_index do |name, number| 
     current_line << " #{number+1}. #{name}" 
     end
