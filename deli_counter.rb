70b08035072ed1c1dd45d59a4686331917b190bf
Sushi Chanrai <sushi@pathworks.co>
2018-06-29 01:33:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+katz_deli = []
+def line (katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    kat_deli.each_with_index { |number, name|} puts "#{number}. #{name}"}
+    #katz_deli.inject { |number,name| puts "The line is currently: #{number+1}. #{name}"}
+  end 
+end
+
+ #1. Logan 2. Avi 3. Spence
