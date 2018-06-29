983d8b3389e15c4dbdab2bedc6136cbe073f605d
Sushi Chanrai <sushi@pathworks.co>
2018-06-29 03:21:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,8 +21,8 @@ end
 
 
 def now_serving
-  if katz_deli > 0
-    puts "Currently serving #{katz_deli"
+  if katz_deli.length > 0
+    puts "Currently serving #{katz_deli.shift}"
   else
     puts "There is nobody waiting to be served!"
   end
